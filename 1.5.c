0988f87ebd6f4247f594405106c776c35d06d085
willcoderwang <wangzhikun@openmailbox.org>
2017-02-26 10:31:47 +0800

    1.5.c

---
+++ 1.5.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+  char c;
+  while((c = getc(stdin)) != EOF)
+    {
+      if(putc(c, stdout) == EOF)
+        {
+          printf("output error!\n");
+          return 1;
+        }
+    }
+  if(ferror(stdin))
+    {
+      printf("input error!\n");
+      return 1;
+    }
+  return 0;
+}
