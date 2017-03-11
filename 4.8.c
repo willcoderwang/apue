371fd75e8c80ce7377756109af028b40b3915f24
willcoderwang <wangzhikun@openmailbox.org>
2017-03-11 23:48:19 +0800

    4.8.c

---
+++ 4.8.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+/* shows the use of access function */
+
+int main(int argc, char *argv[])
+{
+  if(argc != 2)
+    {
+      printf("usage: 4.8 <pathname>\n");
+      return 1;
+    }
+
+  if( access(argv[1], R_OK) < 0)
+    {
+      printf("access error for %s", argv[1]);
+    }
+  else
+    printf("read access OK\n");
+
+  if( open(argv[1], O_RDONLY) < 0)
+    printf("open error for %s", argv[1]);
+  else
+    printf("open for reading OK\n");
+
+  return 0;
+}
